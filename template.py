c19f62a6ed271f6c776df62fe9c041d57b59a902
chrisb <chrism.budnik@gmail.com>
2023-11-21 23:50:00 +0100

    update: fetch last 10 emial msgs

--- template.py
+++ template.py
@@ -28,33 +28,38 @@ def gmail_authenticate():
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
+        
     return build('gmail', 'v1', credentials=creds)
 
-def list_messages(service, user_id):
-    # List all messages of the user's mailbox.
-    response = service.users().messages().list(userId=user_id).execute()
-    messages = []
-    if 'messages' in response:
-        messages.extend(response['messages'])
+def list_messages(service, user_id, max_results=10):
+    # List the last 'max_results' messages of the user's mailbox.
+    response = service.users().messages().list(userId=user_id, maxResults=max_results).execute()
+    messages = response.get('messages', [])
     return messages
 
-def get_message(service, user_id, msg_id):
-    # Get a single message.
-    message = service.users().messages().get(userId=user_id, id=msg_id).execute()
-    return message
+def get_message_header(service, user_id, msg_id):
+    # Get the headers of a single message.
+    message = service.users().messages().get(userId=user_id, id=msg_id, format='metadata').execute()
+    headers = message.get('payload', {}).get('headers', [])
+    header_info = {}
+    for header in headers:
+        if header['name'] in ['Subject', 'From', 'Date']:
+            header_info[header['name']] = header['value']
+    return header_info
 
 def main():
     service = gmail_authenticate()
     messages = list_messages(service, 'me')
-    
+
     emails = []
     for message in messages:
-        msg = get_message(service, 'me', message['id'])
-        # placeholder
-        pass
-
-    df = pd.DataFrame(emails, columns=['Header', 'Sender', 'Content'])
+        header_info = get_message_header(service, 'me', message['id'])
+        emails.append(header_info)
+    
+    df = pd.DataFrame(emails)
     print(df)
 
 if __name__ == '__main__':
     main()
+
+
