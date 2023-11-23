7d531d691dbf2d737560c96273cc9314889b2fd1
chrisb <chrism.budnik@gmail.com>
2023-11-23 20:38:44 +0100

    update: html body method

--- gmail.py
+++ gmail.py
@@ -1,11 +1,8 @@
 import base64
 import os
-import json
 import re
 import pandas as pd
 from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 
 
@@ -42,21 +39,33 @@ class GmailConnector:
         headers = message.get('payload', {}).get('headers', [])
         return {header['name']: header['value'] for header in headers}
 
-    def headers(self, msg_id, user_id: str = "me", default_headers: list = ['Subject', 'From', 'Date']) -> dict:
+    def headers(self, msg_id, user_id: str = "me", selected_headers: list = ['Subject', 'From', 'Date']) -> dict:
         """Get selected the headers of a single message."""
         
         headers = self.all_headers(msg_id, user_id)
-        return {header: headers[header] for header in default_headers}
+        try:
+            output = {header: headers[header] for header in selected_headers}
+        except KeyError:
+            raise KeyError(f"At least one of selected headers not found in message: {headers.keys()}")
+        
+        return output
 
     def text_body(self, msg_id, user_id: str = "me"):
         """Get the body of a single message."""
 
         message = self.service.users().messages().get(userId=user_id, id=msg_id, format='full').execute()
         data = message['payload']['parts'][0]['body']['data']
-        return self._parse_text_plain_message(data)
+        return self._decode_and_transform_text(data)
+    
+    def html_body(self, msg_id, user_id: str = "me"):
+        """Get the body of a single message."""
+
+        message = self.service.users().messages().get(userId=user_id, id=msg_id, format='full').execute()
+        data = message['payload']['parts'][1]['body']['data']
+        return self._decode_and_transform_text(data)
     
     @staticmethod
-    def _parse_text_plain_message(msg: str) -> str:
+    def _decode_and_transform_text(msg: str) -> str:
         """Parse text/plain message."""
 
         # Decoding from base64 URL-safe encoded string
